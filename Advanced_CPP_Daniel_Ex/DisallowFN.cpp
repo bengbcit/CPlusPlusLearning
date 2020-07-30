4c2b3f52cefeddb2d6390c310fcb5663841942c6
bengbcit <bengbcit@gmail.com>
2020-07-29 19:13:53 -0700

    Update

--- Advanced_CPP_Daniel_Ex/DisallowFN.cpp
+++ Advanced_CPP_Daniel_Ex/DisallowFN.cpp
@@ -1,29 +1,18 @@
 #include <iostream>
 using namespace std;
 
-
-//class OpenFile {
-//	/*for c++3, under fns will be disallow in private */
-//	OpenFile(OpenFile& rhs);					// disallow copy constructor
-//	OpenFile& operator= (const OpenFile& rhs);  // disallow copy assignment operator
-//	void writeLine(string str);					// disallow writeLine fn
-//	/*	~OpenFile() destructor in private not always a good idea to put here 
-//		that no destructor will be called might casues leaking resources*/
-//public:
-//	OpenFile(string filename) { cout << "Open a file: " << filename << endl; }
-//	//OpenFile(OpenFile& rhs) = delete;			// c++11: disallow copy constructor 
-//	string g(OpenFile& f) { OpenFile f2(f); }	// these way can access copy constructor f2
-//};
-//
-//int main() {
-//	OpenFile f(string("The Files"));
-//
-//	/*If make a copy constructor will be messy for running time;
-//	so, disallow copy constructor.*/
-//	//OpenFile f2(f);
-//
-//}
-
+class OpenFile_disallow {
+	/*for c++3, under fns will be disallow in private */
+	OpenFile_disallow(OpenFile_disallow& rhs);						// disallow copy constructor
+	//OpenFile_disallow(OpenFile_disallow& rhs) = delete;			// c++11: disallow copy constructor 
+	OpenFile_disallow& operator= (const OpenFile_disallow& rhs);	// disallow copy assignment operator
+	void writeLine(string str);										// disallow writeLine fn
+	/*	~OpenFile_disallow() destructor in private not always a good idea to put here 
+		that no destructor will be called might casues leaking resources*/
+public:
+	OpenFile_disallow(string filename) { cout << "Open a file: " << filename << endl; }
+	string g(OpenFile_disallow& f) { OpenFile_disallow f2(f); }	// these way can access copy constructor f2
+};
 
 // Widely Use: reference counting shared pointers- counts # pointers that points to itself;stop until count to 0
 class OpenFile {// cannot destroyed by others except itself and its relate FNs.
@@ -41,10 +30,14 @@ private:
 };
 
 int main() {
+	OpenFile_disallow f1(string("The Files"));
+	/*If make a copy constructor will be messy for running time; so, disallow copy constructor.*/
+	//OpenFile f2(f);
+
 	//	destructor will fail if OpenFile f is private; but if you create on heap you can use destructor in private
 	//	create obj on heap with NEW:dynamic memory(need a pointer) 
-	OpenFile* f = new OpenFile(string("The Files"));    
-	f->destroyMe();
+	OpenFile* f2 = new OpenFile(string("The Files"));    
+	f2->destroyMe();
 }
 /*	Summary of Disallowing FN:
 		1. c++ 11: f()= delete;
